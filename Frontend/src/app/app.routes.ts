62c9f709c975fbbbdda6a1646c720f1eb9ecc874
Ahmed kallel <113143693+ahmede9ka@users.noreply.github.com>
2025-02-23 14:35:50 +0100

    login

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { RolePickComponent } from './views/role-pick/role-pick.component';
+import { LoginComponent } from './views/login/login.component';
 export const routes: Routes = [
     {
         path: '',
         component: HomePageComponent,
-       
+        
 
     },{
         path: 'chooserole',
         component: RolePickComponent
     },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+
 ];
